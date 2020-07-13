9815a65a1c8f8a36ef7724fd0c3b24aeabec9f8d
juz <electronicallyreached@gmail.com>
2020-07-12 22:37:31 -0500

    adding save book functionality

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { Button } from 'reactstrap'
+import API from '../utils/API'
 
 export default function Book({ title, authors, synopsis, image, link, googleId, id}) {
+
+    function save() {
+        API.saveBook({ title, authors, synopsis, image, link, googleId })
+    }
+
     return (
         <div className="mb-4">
             <img src={image} alt={`${title} cover`}/>
@@ -9,7 +15,7 @@ export default function Book({ title, authors, synopsis, image, link, googleId, 
             <p>{authors}</p>
             <p>{synopsis}</p>
             <a href={link} style={{color: "grey"}} target="_blank" rel="noopener noreferrer">View on Google Books</a>
-            <Button className="btn-sm btn-light btn-outline-secondary ml-4">Save</Button>
+            <Button className="btn-sm btn-light btn-outline-secondary ml-4" onClick={save}>Save</Button>
             <div className="border-bottom mt-2 mb-2"/>
         </div>
     )
