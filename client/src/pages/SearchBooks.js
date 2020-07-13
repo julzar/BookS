ca98d8c663e1ac74b508c9f30eeaf10263906de8
juz <electronicallyreached@gmail.com>
2020-07-12 22:14:10 -0500

    added searchbooks

---
+++ client/src/pages/SearchBooks.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react'
+
+import { Container } from 'reactstrap'
+
+import API from '../utils/API'
+
+import Search from '../components/Search'
+import Book from '../components/Book'
+import NotFound from '../components/NotFound'
+
+export default function SearchBooks() {
+    const [books, setBooks] = useState([])
+    const [search, setSearch] = useState('')
+
+    useEffect(() => {
+        if (search) {
+          API.searchBooks(search).then((res) => {
+            setBooks(res.data.items);
+          })
+        }
+      }, [search]);
+
+    function renderBooks() {
+        if (!books) {
+            return (
+                <NotFound />
+            )
+        }
+        return books.map(({ volumeInfo, id }, i) => {
+            return (
+                <Book
+                    key={i}
+                    googleId={id}
+                    title={volumeInfo.title}
+                    authors={(volumeInfo.authors ? volumeInfo.authors.join(', ') : 'No author')}
+                    link={volumeInfo.infoLink}
+                    image={(volumeInfo.imageLinks ? volumeInfo.imageLinks.thumbnail : 'No image')}
+                    synopsis={(volumeInfo.description || 'No synopsis')}
+                />
+            )
+        })
+    }
+
+    function handleChange(e) {
+        const { value } = e.target
+        setSearch(value.trim())
+    }
+
+    return (
+        <div>
+            {/* <Container> */}
+            <Search handleChange={handleChange} />
+            <div>
+            {renderBooks()}
+            </div>
+            {/* </Container> */}
+        </div>
+    )
+}
